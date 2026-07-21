ed29f979c77bf7c0eb1525df730482d304878386
Zehao Yu <interstellartrek@outlook.com>
2026-07-21 10:39:05 -0700

    math tools added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import remarkMath from 'remark-math'; // [新增]
+import rehypeKatex from 'rehype-katex'; // [新增]
 
-// https://astro.build/config
 export default defineConfig({
-  // 👇 这里已经改成了你的专属地址
-  site: 'https://interstellartrek.github.io', 
+  site: 'https://interstellartrek.github.io',
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMath], // [新增]
+    rehypePlugins: [rehypeKatex], // [新增]
+  },
 });
